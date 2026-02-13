92151db8fd063e72bbd0e3cd2972ba82932133e4
Anshu Kumar Gupta <anshuprojectbill9862@gmail.com>
2026-02-13 20:33:37 +0530

    feat: integrate notification feature with firestore backend

--- app/src/main/java/com/anshu/collegemate/Data/Repository/AnnouncementRepository.kt
+++ app/src/main/java/com/anshu/collegemate/Data/Repository/AnnouncementRepository.kt
@@ -1,26 +1,24 @@
 package com.anshu.collegemate.Data.Repository
 
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import com.anshu.collegemate.Data.Injections.FireStoreInjection
 import com.anshu.collegemate.Data.Model.Announcement.AnnouncementCard
 import com.anshu.collegemate.Utils.DateTimeUtil
-import com.anshu.collegemate.ui.View.Others.MBS.AnnouncementMBS
 import kotlinx.coroutines.tasks.await
 
 class AnnouncementRepository {
-    val result = mutableListOf<AnnouncementCard>(AnnouncementCard(message = "Test1"), AnnouncementCard(message = "Test2"))
     val firestore = FireStoreInjection.getFirestore()
     fun saveAnnouncementInFireStore(a: AnnouncementCard){
-        result.add(a)
-//        firestore.collection("Announcement").add(a)
-//            .addOnSuccessListener { doc->
-//                firestore.collection("Announcement")
-//                    .document(doc.id).update("id",doc.id)
-//            }
-//            .addOnFailureListener { e->
-//                Log.e("Error",e.toString())
-//            }
+        firestore.collection("Announcement").add(a)
+            .addOnSuccessListener { doc->
+                firestore.collection("Announcement")
+                    .document(doc.id).update("id",doc.id)
+            }
+            .addOnFailureListener { e->
+                Log.e("Error",e.toString())
+            }
     }
 
 
@@ -40,16 +38,15 @@ class AnnouncementRepository {
         }
     }
     suspend fun getAnnouncement():List<AnnouncementCard>{
-        return result
-//        return try {
-//            val snapshots = firestore.collection("Announcement").orderBy("createdAt").get().await()
-//            snapshots.documents.mapNotNull { doc->
-//                doc.toObject(AnnouncementCard::class.java)
-//            }
-//        }
-//        catch (e: Exception){
-//            return emptyList()
-//        }
+        return try {
+            val snapshots = firestore.collection("Announcement").orderBy("createdAt").get().await()
+            snapshots.documents.mapNotNull { doc->
+                doc.toObject(AnnouncementCard::class.java)
+            }
+        }
+        catch (e: Exception){
+            return emptyList()
+        }
     }
 
 }
