dfb282d38b1f0d7fb2b58748e298fdc936738155
Anshu Kumar Gupta <anshuprojectbill9862@gmail.com>
2026-03-10 20:30:38 +0530

    feat: Improved AssignmentTestMBS.kt on KAIZEN principle

--- app/src/main/java/com/anshu/collegemate/ui/View/Others/MBS/AssignmentTestMBS.kt
+++ app/src/main/java/com/anshu/collegemate/ui/View/Others/MBS/AssignmentTestMBS.kt
@@ -1,8 +1,11 @@
 package com.anshu.collegemate.ui.View.Others.MBS
 
+import android.net.wifi.p2p.WifiP2pGroup
 import android.os.Build
+import android.sax.RootElement
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -25,24 +28,30 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.traceEventStart
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.core.content.contentValuesOf
 import com.anshu.collegemate.Data.Model.AssignmentTest.AssignmentCard
 import com.anshu.collegemate.Data.Model.AssignmentTest.TestCard
 import com.anshu.collegemate.Data.Model.HomeScreen.RoutineSeed
+import com.anshu.collegemate.Data.Model.HomeScreen.ScheduleCardData
 import com.anshu.collegemate.R
+import com.anshu.collegemate.Utils.DateTimeUtil
 import com.anshu.collegemate.ui.ViewModel.AssignmentTestVM
 import com.anshu.collegemate.ui.ViewModel.UserViewModel
 import java.time.Instant
 import java.time.ZoneId
 import java.util.concurrent.TimeUnit
+
 enum class steps{
     TYPE,SUBJECT,DETAILS,REVIEW
 }
-enum class type{
+enum class types{
     TEST,ASSIGNMENT
 }
 
@@ -50,58 +59,60 @@ enum class type{
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AssignmentTestMBS(assTesVM: AssignmentTestVM, onDismiss:()->Unit) {
-    var selectedButton by remember { mutableStateOf("NONE") }
     var subjectName by remember { mutableStateOf("") }
     var subjectCode by remember { mutableStateOf("") }
-    var questionText by remember { mutableStateOf("Either u can add text/pdf/image of the question. Write for question here.") }
+    //contentText works both for test Assignment in TextField
+    var contentText by remember { mutableStateOf("") }
     var showDatePicker by remember { mutableStateOf(false) }
     var lastDateOfAssignment by remember{mutableStateOf(0L)}
     val datePickerState = rememberDatePickerState()
     var createdAt by remember { mutableStateOf(0L) }
     var createdBy by remember { mutableStateOf("") }
     var testDate by remember {mutableStateOf(0L)}
-    var testSyllabus by remember { mutableStateOf("Enter Topics or Syllabus for the this Test(Optional)") }
+    //var testSyllabus by remember { mutableStateOf("Enter Topics or Syllabus for the this Test(Optional)") }
     val context =   LocalContext.current
+    var currentStep by remember { mutableStateOf(steps.TYPE) }
+    var type by remember { mutableStateOf<types?>(null) }
 
     ModalBottomSheet(onDismissRequest = { onDismiss() }) {
 
-
-        Column(modifier = Modifier.padding(10.dp)) {
-            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
-                Button(onClick = {
-                    selectedButton = "TEST"
-                }) { Text("Test") }
-                Button(onClick = {
-                    selectedButton = "ASSIGNMENT"
-                }) { Text("ASSIGNMENT") }
-
+        when(currentStep){
+            steps.TYPE->{
+                TypeStep {
+                    type = it
+                    currentStep = steps.SUBJECT
+                }
             }
-            when (selectedButton) {
-                "TEST" -> {
-                    Text("Please Choose Subject!")
-                    LazyColumn() {
-                        items(RoutineSeed.setOfDistinctClasses) {
-                            Button(onClick = {
-                                subjectCode = it.subjectCode
-                                subjectName = it.name
-                            }) {
-                                Text(it.name)
-                            }
-                        }
+            steps.SUBJECT->{
+                SubjectStep(RoutineSeed.setOfDistinctClasses) {
+                    sc->
+                    subjectName = sc.name
+                    subjectCode = sc.subjectCode
+                    currentStep = steps.DETAILS
+                }
+            }
+            steps.DETAILS->{
+                when(type){
+                    types.TEST -> {
+                        TestDetailsStep(type!!,subjectName,contentText,testDate,
+                            onSyllabusChange = {contentText=it}, onCalenderClicked = {
+                                showDatePicker = true}, onNextClicked = {currentStep = steps.REVIEW})
                     }
-                    Row(){Text("When is the Test?")
-                        Button(onClick = {
-                            showDatePicker=true}) {
-                            Text("📅")
-                        }
-                    }
-                    OutlinedTextField(value = testSyllabus, onValueChange = {testSyllabus=it},
-                        label = {Text("Syllabus/Topics")})
-
-                    //TODO expiry At plus day one to testDate use it directly while creating object of Test
-                    Row() {
-                        Button(onClick = {
-                            createdBy = UserViewModel.userP.value?.name?:""
+                    types.ASSIGNMENT -> {AssignmentDetailsStep(
+                        type!!,subjectName,contentText,lastDateOfAssignment,
+                        onQuestionChange = {contentText = it}, onCalenderClicked = {
+                            showDatePicker = true }, onNextClicked = {
+                            currentStep = steps.REVIEW
+                        })}
+                    else -> {}
+                }
+            }
+            steps.REVIEW->{
+                when(type){
+                    types.TEST->{
+                        ReviewTestDetail(subjectName,subjectCode,contentText,testDate,
+                            onConfirm = {
+                                createdBy = UserViewModel.userP.value?.name?:""
                             createdAt = System.currentTimeMillis()
                             val tc = TestCard(
                                 subjectName = subjectName,
@@ -110,66 +121,26 @@ fun AssignmentTestMBS(assTesVM: AssignmentTestVM, onDismiss:()->Unit) {
                                 createdBy = createdBy,
                                 testDate = testDate,
                                 expiryAt = testDate + TimeUnit.DAYS.toMillis(1),
-                                syllabus = testSyllabus
+                                syllabus = contentText
                             )
                             assTesVM.addTest(tc)
                             Toast.makeText(context,"Test successfully added",Toast.LENGTH_LONG).show()
                             onDismiss()
-                        }) {
-                            Text("Confirm")
-                        }
-                        Button(onClick = {onDismiss()}) {
-                            Text("Cancel")
-                        }
-
+                            },
+                            onCancel = {
+                                onDismiss()
+                            })
                     }
-
-
-                }
-
-                "ASSIGNMENT" -> {
-                    Text("Please Choose Subject!")
-                    LazyColumn(horizontalAlignment = Alignment.End) {
-                        items(RoutineSeed.setOfDistinctClasses) {
-                            Button(onClick = {
-                                subjectCode = it.subjectCode
-                                subjectName = it.name
-                            }) {
-                                Text(it.name)
-                            }
-                        }
-                    }
-                    OutlinedTextField(
-                        onValueChange = { questionText = it }, value = questionText,
-                        label = { Text("Assignment Question") })
-                    Row(modifier = Modifier.fillMaxWidth()) {
-                        IconButton(onClick = {}) {
-                            Icon(
-                                painter = painterResource(R.drawable.image_24px),
-                                contentDescription = null
-                            )
-                        }
-                        IconButton(onClick = {}) {
-                            Icon(
-                                painter = painterResource(R.drawable.picture_as_pdf_24px),
-                                contentDescription = null
-                            )
-                        }
-
-                    }
-                    Row(){Text("Choose Last Date of submission")
-                        Button(onClick = {showDatePicker=true}) {
-                            Text("📅")
-                        }
-                    }
-                    Row() {
-                        Button(onClick = {
+                    types.ASSIGNMENT->{
+                        ReviewAssignmentDetail(subjectName,subjectCode,contentText,lastDateOfAssignment,
+                            onConfirm =
+                                {
                             createdBy = UserViewModel.userP.value?.name?:""
                             createdAt = System.currentTimeMillis()
                             val ass = AssignmentCard(
                                 subjectName = subjectName,
                                 subjectCode = subjectCode,
-                                questionText = questionText,
+                                questionText = contentText,
                                 createdBy = createdBy,
                                 createdAt = createdAt,
                                 expiryAt = lastDateOfAssignment + TimeUnit.DAYS.toMillis(1),
@@ -178,18 +149,19 @@ fun AssignmentTestMBS(assTesVM: AssignmentTestVM, onDismiss:()->Unit) {
                             assTesVM.addAssignment(ass)
                             Toast.makeText(context,"Assignment Successfully Added!",Toast.LENGTH_LONG).show()
                             onDismiss()
-                        }) {
-                            Text("Confirm")
-                        }
-                        Button(onClick = {onDismiss()}) {
-                            Text("Cancel")
-                        }
 
+                            },
+                            onCancel = {
+                                onDismiss()
+                            })
                     }
-
+                    else -> {}
                 }
+
             }
         }
+
+
 
    }
     if (showDatePicker){
@@ -206,7 +178,7 @@ fun AssignmentTestMBS(assTesVM: AssignmentTestVM, onDismiss:()->Unit) {
                         .atZone(ZoneId.systemDefault())
                         .toInstant()
                         .toEpochMilli()
-                    if (selectedButton=="ASSIGNMENT")lastDateOfAssignment = endOfDayMillis
+                    if (type.toString()=="ASSIGNMENT")lastDateOfAssignment = endOfDayMillis
                     else {
                         testDate=endOfDayMillis
                     }
@@ -221,4 +193,201 @@ fun AssignmentTestMBS(assTesVM: AssignmentTestVM, onDismiss:()->Unit) {
             DatePicker(state = datePickerState)
         }
     }
+}
+@Composable
+fun TypeStep(
+    onTypeSelected: (types) -> Unit
+) {
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+
+        Text("What do you want to add?")
+
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
+
+            Button(onClick = {
+                onTypeSelected(types.TEST)
+            }) {
+                Text("Test")
+            }
+
+            Button(onClick = {
+                onTypeSelected(types.ASSIGNMENT)
+            }) {
+                Text("Assignment")
+            }
+
+        }
+    }
+}
+@Composable
+fun SubjectStep(
+    subjects: List<ScheduleCardData>,
+    onSubjectSelect:(sc: ScheduleCardData)->Unit
+) {
+    Column() {
+        Text("Choose Subject")
+    LazyColumn() {
+
+        items(subjects) { subject ->
+            Row(
+                modifier = Modifier.fillMaxWidth()
+                .padding(25.dp)
+                .clickable(onClick = { onSubjectSelect(subject) })
+            )
+            {
+                Text(subject.name)
+            }
+        }
+    }
+}
+}
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun AssignmentDetailsStep(
+    type: types,
+    subjectName: String,
+    question:String,
+    lastDate:Long,
+    onQuestionChange:(newValue:String)->Unit,
+    onCalenderClicked:()->Unit,
+    onNextClicked:()-> Unit
+){
+    Column() {
+        Text(type.toString()+"."+subjectName)
+
+        OutlinedTextField(value = question, onValueChange = {onQuestionChange(it)}
+            , label ={Text("Question")})
+        Row(modifier = Modifier.fillMaxWidth()) {
+                        IconButton(onClick = {}) {
+                            Icon(
+                                painter = painterResource(R.drawable.image_24px),
+                                contentDescription = null
+                            )
+                        }
+                        IconButton(onClick = {}) {
+                            Icon(
+                                painter = painterResource(R.drawable.picture_as_pdf_24px),
+                                contentDescription = null
+                            )
+                        }
+
+                    }
+        Row() {
+            Text("Last Date")
+            Button(onClick = {
+                onCalenderClicked()}) {
+                if(lastDate==0L){
+                    Text("📅 Select Date")
+                }
+                else{
+                    Text("📅 ${DateTimeUtil.getDateMonthFromLong(lastDate)}")
+                }
+            }
+
+        }
+        Button(onClick = {onNextClicked()}) {
+            Text("Next->")
+        }
+
+    }
+}
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun TestDetailsStep(
+    type: types,
+    subjectName: String,
+    syllabus:String,
+    testDate:Long,
+    onSyllabusChange:(newValue: String)->Unit,
+    onCalenderClicked:()-> Unit,
+    onNextClicked:()->Unit
+){
+    Column() {
+        Text(type.toString()+"."+subjectName)
+
+        OutlinedTextField(value = syllabus, onValueChange = {onSyllabusChange(it)}
+            , label ={Text("Question")})
+        Row(modifier = Modifier.fillMaxWidth()) {
+            IconButton(onClick = {}) {
+                Icon(
+                    painter = painterResource(R.drawable.image_24px),
+                    contentDescription = null
+                )
+            }
+            IconButton(onClick = {}) {
+                Icon(
+                    painter = painterResource(R.drawable.picture_as_pdf_24px),
+                    contentDescription = null
+                )
+            }
+
+        }
+        Row() {
+            Text("Last Date")
+            Button(onClick = {
+                onCalenderClicked()}) {
+                if(testDate ==0L){
+                    Text("📅 Select Date")
+                }
+                else{
+                    Text("📅 ${DateTimeUtil.getDateMonthFromLong(testDate)}")
+                }
+            }
+
+        }
+        Button(onClick = {onNextClicked()}) {
+            Text("Next->")
+        }
+
+    }
+}
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun ReviewTestDetail(
+    subjectName: String,
+    subjectCode: String,
+    syllabus: String,
+    testDate:Long,
+    onConfirm:()->Unit,
+    onCancel:()-> Unit
+){
+            Column(Modifier.padding(10.dp)) {
+                Text("TEST . $subjectName$subjectCode")
+                Text(syllabus)
+                Text("Attachment Included")
+                Text(DateTimeUtil.getDateMonthFromLong(testDate))
+                Button(onClick = onConfirm) {
+                    Text("Confirm")
+                }
+                Button(onClick = onCancel) {
+                    Text("Cancel")
+                }
+
+            }
+
+}
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun ReviewAssignmentDetail(
+    subjectName: String,
+    subjectCode: String,
+    question: String,
+    lastDate: Long,
+    onConfirm:()->Unit,
+    onCancel:()-> Unit
+){
+    Column(Modifier.padding(10.dp)) {
+        Text("ASSIGNMENT . "+subjectName+subjectCode)
+        Text(question)
+        Text("Attachment Included")
+        Text(DateTimeUtil.getDateMonthFromLong(lastDate))
+        Button(onClick = onConfirm) {
+            Text("Confirm")
+        }
+        Button(onClick = onCancel) {
+            Text("Cancel")
+        }
+    }
+
 }
